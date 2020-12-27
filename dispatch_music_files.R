5f8c12d200606f57e5c57c4f3ba24a8cbc273990
bob <antoinelanguillaume@gmail.com>
2020-12-27 17:53:06 +0100

    Update data output directory name to match Itunes file structure

--- dispatch_music_files.R
+++ dispatch_music_files.R
@@ -10,7 +10,7 @@ raw_music_files = lapply(list_cd_dirs("raw"),
                                       pattern = "\\.m4a$",
                                       full.names = TRUE)
                          })
-target_dirs = list_cd_dirs("updated")
+target_dirs = list_cd_dirs("Léo Férré")
 
 purrr::walk2(raw_music_files, target_dirs,
              function(files, dir) {
@@ -20,5 +20,5 @@ purrr::walk2(raw_music_files, target_dirs,
 
 # orginal_names = list.files(target_dirs[[2]], pattern = "\\.m4a$")
 # new_names = sub("(^\\d{2}).+(\\.m4a)", "\\1_cd1\\2", music_files)
-# file.rename(from = file.path("music/updated/cd-1/", orginal_names),
-#             to = file.path("music/updated/cd-1/", new_names))
+# file.rename(from = file.path("music/Léo Férré/cd-1/", orginal_names),
+#             to = file.path("music/Léo Férré/cd-1/", new_names))
--- update_tag_id3.py
+++ update_tag_id3.py
@@ -2,7 +2,7 @@ import os
 import mutagen
 
 def read_songs(cd_number):
-  m4a_files = os.listdir(f'music/updated/cd-{cd_number}/')
+  m4a_files = os.listdir(f'music/Léo Férré/cd-{cd_number}/')
   m4a_files.sort()
   song_names = open(f'text/cd{cd_number}.txt', 'r').read().split('\n')
   songs = dict(zip(m4a_files, song_names))
@@ -11,13 +11,13 @@ def read_songs(cd_number):
 def update_songs_metadata(cd_number):
   songs = read_songs(cd_number)
   for file, name in songs.items():
-    path_file = os.path.join(f'music/updated/cd-{cd_number}/', file)
+    path_file = os.path.join(f'music/Léo Férré/cd-{cd_number}/', file)
     file = mutagen.File(path_file)
     file['©nam'] = name
     file['©ART'] = 'Léo Férré'
     file['©alb'] = 'Anthologie'
     file.save()
-    os.rename(path_file, f'music/updated/cd-{cd_number}/{name}_cd{cd_number}.m4a')
+    os.rename(path_file, f'music/Léo Férré/cd-{cd_number}/{name}_cd{cd_number}.m4a')
     
     
 cd_numbers= [1, 2, 3, 4]
